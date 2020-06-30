b0cf64fa2cfd61aeff97d2c633aa310894c33066
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-06-30 15:24:59 -0430

    styles header

---
+++ src/containers/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+/*____ Components ____*/
+import Header from "../components/Header";
+import "../assets/styles/App.scss";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-/*____ Components ____*/
-import HelloWorld from "./components/HelloWorld";
+/*____ Container ____*/
+import App from "./containers/App";
 
-ReactDOM.render(<HelloWorld />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
